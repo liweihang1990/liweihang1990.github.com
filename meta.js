931afd94c42785a530126e44bebeebad1d41bc79
liweihang1990 <liweihang_1990@126.com>
2012-07-18 10:58:39 +0800

    Update master

--- meta.js
+++ meta.js
@@ -2,14 +2,18 @@
   "site_name":"weihang.me",
   "copyright":"wawasoft 2012",
   "cates":[
-    {"name":"tech","text":"技术"},
+    {"name":"task","text":"技术"},
     {"name":"art","text":"艺术"},
     {"name":"other","text":"其他"}
   ],
   "articles":[
-    {"title": "哪段代码更Pythonic呢？", "file": "2012-03-02-witch-one-is-best-pythonic", "cate":"tech"},
-    {"title": "发一组人像", "file": "2012-03-17-portrait-5018", "cate":"art"},
-    {"title": "发一组猫咪照", "file": "2012-03-20-cat-5018", "cate":"art"},
-    {"title": "微博摘录2012-02", "file": "2012-02-28-weibo", "cate":"other"}
+    {"title": "Task1", "file": "Tasks1", "cate":"task"},
+    {"title": "Task2", "file": "Tasks2", "cate":"task"},
+    {"title": "Task3", "file": "Tasks3", "cate":"task"},
+    {"title": "Task4", "file": "Tasks4(Trigger on Contact)", "cate":"task"}
+    {"title": "Task5", "file": "Tasks5(Pattern & Matcher)", "cate":"task"}
+    {"title": "Task6", "file": "Tasks6(Apex managed sharing & Batch & Scheduler & SendEmail)", "cate":"task"}
+    {"title": "Task7", "file": "Tasks7(VF Page for Contact Management with sorting & paging on server side)", "cate":"task"}
+    
   ]
 }
