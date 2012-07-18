41d416b060b48dce2dae921a4244f58bb27dad8f
liweihang1990 <liweihang_1990@126.com>
2012-07-18 11:19:07 +0800

    Update master

--- meta.js
+++ meta.js
@@ -5,13 +5,13 @@
     {"name":"task","text":"技术"}
   ],
   "articles":[
-    {"title": "Task1", "file": "2012-07-18Tasks1.md", "cate":"task"},
-    {"title": "Task2", "file": "2012-07-18Tasks2.md", "cate":"task"},
-    {"title": "Task3", "file": "2012-07-18Tasks3.md", "cate":"task"},
-    {"title": "Task4", "file": "2012-07-18Tasks4(Trigger on Contact).md", "cate":"task"}
-    {"title": "Task5", "file": "2012-07-18Tasks5(Pattern & Matcher).md", "cate":"task"}
-    {"title": "Task6", "file": "2012-07-18Tasks6(Apex managed sharing & Batch & Scheduler & SendEmail).md", "cate":"task"}
-    {"title": "Task7", "file": "2012-07-18Tasks7(VF Page for Contact Management with sorting & paging on server side).md", "cate":"task"}
-    
+    {"title": "Task1", "file": "2012-07-18Tasks1", "cate":"task"},
+    {"title": "Task2", "file": "2012-07-18Tasks2", "cate":"task"},
+    {"title": "Task3", "file": "2012-07-18Tasks3", "cate":"task"},
+    {"title": "Task4", "file": "2012-07-18Tasks4(Trigger on Contact)", "cate":"task"},
+    {"title": "Task5", "file": "2012-07-18Tasks5(Pattern & Matcher)", "cate":"task"},
+    {"title": "Task6", "file": "2012-07-18Tasks6(Apex managed sharing & Batch & Scheduler & SendEmail)", "cate":"task"},
+    {"title": "Task7", "file": "2012-07-18Tasks7(VF Page for Contact Management with sorting & paging on server side)", "cate":"task"},
+    {"title": "Test", "file": "2012-03-20-cat-5018.txt", "cate":"task"}
   ]
 }
